bc5f605adb7dfb8b9f0f49e1cd579595ba4367ba
Mauro Melo <mauromelo1@gmail.com>
2021-03-15 15:04:22 -0300

    Category

---
+++ src/main/java/com/mauromelo1/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.mauromelo1.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mauromelo1.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
